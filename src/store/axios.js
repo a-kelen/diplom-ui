26fba6643298945d2e5f106ca57683cfadf3f7d2
a-kelen <39244363+a-kelen@users.noreply.github.com>
2021-06-08 19:50:59 +0300

    deploy #1

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,8 +1,14 @@
 import Axios from 'axios'
 import https from 'https' 
-
+let url = ""
+if(process.env.NODE_ENV == 'development')  {
+    url = 'https://localhost:44345/api/';
+}
+else
+    url = 'http://akelen-001-site1.itempurl.com/'
+    
 const api = Axios.create({
-    baseURL: 'https://localhost:44345/api/',
+    baseURL: url,
     httpsAgent: new https.Agent({
         rejectUnauthorized: false
     })
