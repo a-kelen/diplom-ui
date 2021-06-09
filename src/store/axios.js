21f267ffd994edea3e0008f8ee11e6d869b3d961
a-kelen <39244363+a-kelen@users.noreply.github.com>
2021-06-09 17:23:08 +0300

    deploy#6

--- src/store/axios.js
+++ src/store/axios.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV == 'development')  {
     url = 'https://localhost:44345/api/';
 }
 else
-    url = 'https://composdevapi.herokuapp.com/api/'
+    url = 'https://u1400620.plsk.regruhosting.ru/api/'
 
 console.log(process.env)
 
